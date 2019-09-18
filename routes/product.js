69b03325ab4a56e2cb6083444a330a15e273bb07
luck_body <707798472@qq.com>
2019-09-18 22:30:54 +0800

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -3,18 +3,48 @@ var router = express.Router()
 var db = require("../utils/db")
 
 router.get("/list", function(req, res, next) {
-  db.query("select * from product", function(err, data) {
-    if (!err) {
-      res.send({
-        code: 200,
-        data
-      })
-    } else {
-      res.send({
-        code: 500,
-        status: "error"
-      })
-    }
-  })
+  //   console.log(req.query)
+  const { pageNo, pageSize } = req.query
+  let sql = ""
+  let response = {}
+  if (pageNo) {
+    db.query(
+      `select SQL_CALC_FOUND_ROWS * from product limit ${(Number(pageNo) - 1) *
+        Number(pageSize)}, ${Number(pageSize)};`,
+      (err, data) => {
+        if (!err) {
+          response = data
+          db.query(`SELECT FOUND_ROWS() as total;`, (err, data) => {
+            res.send({
+              code: 200,
+              data: response,
+              total: data[0].total
+            })
+          })
+        } else {
+          res.send({
+            code: 500
+          })
+        }
+      }
+    )
+  } else {
+    sql = `select * from product`
+    console.log(sql)
+    db.query(sql, function(err, data) {
+      console.log(err)
+      if (!err) {
+        res.send({
+          code: 200,
+          data
+        })
+      } else {
+        res.send({
+          code: 500,
+          status: "error"
+        })
+      }
+    })
+  }
 })
 module.exports = router
